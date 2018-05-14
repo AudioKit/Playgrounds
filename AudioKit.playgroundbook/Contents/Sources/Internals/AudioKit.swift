d52370ddf02d56de931bb7b84f4df00c3a75c0a6
Aurelius Prochazka <aurephd@gmail.com>
2018-05-14 02:12:17 -0700

    Adding in the broken up AudioKit.swift

--- AudioKit.playgroundbook/Contents/Sources/Internals/AudioKit.swift
+++ AudioKit.playgroundbook/Contents/Sources/Internals/AudioKit.swift
@@ -3,286 +3,150 @@
 //  AudioKit
 //
 //  Created by Aurelius Prochazka, revision history on Github.
-//  Copyright © 2016 AudioKit. All rights reserved.
+//  Copyright © 2018 AudioKit. All rights reserved.
 //
 
 import Foundation
 import AVFoundation
 
-public typealias AKCallback = (Void) -> Void
+
+import CoreAudioKit
+import UIKit
+import Dispatch
+
+public typealias AKCallback = () -> Void
 
 /// Top level AudioKit managing class
 @objc open class AudioKit: NSObject {
 
-    // MARK: Global audio format (44.1K, Stereo)
-
-    /// Format of AudioKit Nodes
-    open static var format = AKSettings.audioFormat
-
     // MARK: - Internal audio engine mechanics
 
     /// Reference to the AV Audio Engine
-    open static let engine = AVAudioEngine()
+    @objc public static let engine = AVAudioEngine()
 
-    static var shouldBeRunning = false
+    @objc static var finalMixer = AKMixer()
 
-    /// An audio output operation that most applications will need to use last
-    open static var output: AKNode? {
-        didSet {
-            engine.connect(output!.avAudioNode,
-                           to: engine.outputNode,
-                           format: AudioKit.format)
-        }
-    }
-    
     // MARK: - Device Management
 
-    /// Enumerate the list of available input devices.
-    open static var availableInputs: [AKDevice]? {
-        #if os(OSX)
-            EZAudioUtilities.setShouldExitOnCheckResultFail(false)
-            return EZAudioDevice.inputDevices().map {
-                AKDevice(name: ($0 as AnyObject).name, deviceID: ($0 as AnyObject).deviceID)
+    /// An audio output operation that most applications will need to use last
+    @objc open static var output: AKNode? {
+        didSet {
+            do {
+                try updateSessionCategoryAndOptions()
+                output?.connect(to: finalMixer)
+                engine.connect(finalMixer.avAudioNode, to: engine.outputNode)
+
+                } catch {
+                AKLog("Could not set output: \(error)")
             }
-        #else
-            if let devices = AVAudioSession.sharedInstance().availableInputs {
-                return devices.map {
-                    AKDevice(name: $0.portName, deviceID: $0.uid)
-                }
-            }
-            return nil
-        #endif
-    }
-    /// Enumerate the list of available output devices.
-    open static var availableOutputs: [AKDevice]? {
-        #if os(OSX)
-            EZAudioUtilities.setShouldExitOnCheckResultFail(false)
-            return EZAudioDevice.outputDevices().map {
-                AKDevice(name: ($0 as AnyObject).name, deviceID: ($0 as AnyObject).deviceID)
-            }
-        #else
-            return nil
-        #endif
+        }
     }
 
-    /// The name of the current preferred input device, if available.
-    open static var inputDevice: AKDevice? {
-        #if os(OSX)
-            if let dev = EZAudioDevice.currentInput() {
-                return AKDevice(name: dev.name, deviceID: dev.deviceID)
+    /// Enumerate the list of available input devices.
+    @objc open static var inputDevices: [AKDevice]? {
+
+        var returnDevices = [AKDevice]()
+        if let devices = AVAudioSession.sharedInstance().availableInputs {
+            for device in devices {
+                if device.dataSources == nil || device.dataSources!.isEmpty {
+                    returnDevices.append(AKDevice(name: device.portName, deviceID: device.uid))
+                } else {
+                    for dataSource in device.dataSources! {
+                        returnDevices.append(AKDevice(name: device.portName,
+                                                        deviceID: "\(device.uid) \(dataSource.dataSourceName)"))
+                    }
+                }
             }
-        #else
-            if let dev = AVAudioSession.sharedInstance().preferredInput {
-                return AKDevice(name: dev.portName, deviceID: dev.uid)
-            }
-        #endif
+            return returnDevices
+        }
         return nil
     }
 
-    /// Change the preferred input device, giving it one of the names from the list of available inputs.
-    open static func setInputDevice(_ input: AKDevice) throws {
-        #if os(OSX)
-            var address = AudioObjectPropertyAddress(
-                mSelector: kAudioHardwarePropertyDefaultInputDevice,
-                mScope: kAudioObjectPropertyScopeGlobal,
-                mElement: kAudioObjectPropertyElementMaster)
-            var devid = input.deviceID
-            AudioObjectSetPropertyData(
-                AudioObjectID(kAudioObjectSystemObject),
-                &address, 0, nil, UInt32(MemoryLayout<AudioDeviceID>.size), &devid)
-        #else
-            if let devices = AVAudioSession.sharedInstance().availableInputs {
-                for dev in devices {
-                    if dev.uid == input.deviceID {
-                        try AVAudioSession.sharedInstance().setPreferredInput(dev)
-                    }
+    /// Enumerate the list of available output devices.
+    @objc open static var outputDevices: [AKDevice]? {
+        let devs = AVAudioSession.sharedInstance().currentRoute.outputs
+        if devs.isNotEmpty {
+            var outs = [AKDevice]()
+            for dev in devs {
+                outs.append(AKDevice(name: dev.portName, deviceID: dev.uid))
+            }
+            return outs
+        }
+        return nil
+    }
+
+    /// The name of the current input device, if available.
+    @objc open static var inputDevice: AKDevice? {
+        if let dev = AVAudioSession.sharedInstance().preferredInput {
+            return AKDevice(name: dev.portName, deviceID: dev.uid)
+        } else {
+            let inputDevices = AVAudioSession.sharedInstance().currentRoute.inputs
+            if inputDevices.isNotEmpty {
+                for device in inputDevices {
+                    let dataSourceString = device.selectedDataSource?.description ?? ""
+                    let id = "\(device.uid) \(dataSourceString)".trimmingCharacters(in: [" "])
+                    return AKDevice(name: device.portName, deviceID: id)
                 }
             }
-        #endif
-    }
-    
-    /// Change the preferred output device, giving it one of the names from the list of available output.
-    open static func setOutputDevice(_ output: AKDevice) throws {
-        #if os(OSX)
-            var address = AudioObjectPropertyAddress(
-                mSelector: kAudioHardwarePropertyDefaultOutputDevice,
-                mScope: kAudioObjectPropertyScopeGlobal,
-                mElement: kAudioObjectPropertyElementMaster)
-            var devid = output.deviceID
-            AudioObjectSetPropertyData(
-                AudioObjectID(kAudioObjectSystemObject),
-                &address, 0, nil, UInt32(MemoryLayout<AudioDeviceID>.size), &devid)
-        #else
-            //not available on ios
-        #endif
-    }
-
-    // MARK: - Start/Stop
-    
-    /// Start up the audio engine
-    open static func start() {
-        if output == nil {
-            NSLog("AudioKit: No output node has been set yet, no processing will happen.")
         }
-        // Start the engine.
-        do {
-            self.engine.prepare()
+        return nil
+    }
 
-            #if os(iOS)
+    /// The name of the current output device, if available.
+    @objc open static var outputDevice: AKDevice? {
+        let devs = AVAudioSession.sharedInstance().currentRoute.outputs
+        if devs.isNotEmpty {
+            return AKDevice(name: devs[0].portName, deviceID: devs[0].uid)
+        }
 
-                NotificationCenter.default.addObserver(
-                    self,
-                    selector: #selector(AudioKit.restartEngineAfterRouteChange(_:)),
-                    name: NSNotification.Name.AVAudioSessionRouteChange,
-                    object: nil)
-            #endif
-            #if !os(OSX)
-                if AKSettings.audioInputEnabled {
+        return nil
+    }
 
-                #if os(iOS)
-                    if AKSettings.defaultToSpeaker {
-                        try AKSettings.setSession(category: .playAndRecord,                                                           with: .mixWithOthers)
+        /// Change the preferred input device, giving it one of the names from the list of available inputs.
+    @objc open static func setInputDevice(_ input: AKDevice) throws {
 
-                        // listen to AVAudioEngineConfigurationChangeNotification
-                        // and restart the engine if it is stopped.
-                        NotificationCenter.default.addObserver(
-                            self,
-                            selector: #selector(AudioKit.audioEngineConfigurationChange(_:)),
-                            name: NSNotification.Name.AVAudioEngineConfigurationChange,
-                            object: engine)
-
-                    } else {
-
-                        try AKSettings.setSession(category: .playAndRecord)
-
+        if let devices = AVAudioSession.sharedInstance().availableInputs {
+            for device in devices {
+                if device.dataSources == nil || device.dataSources!.isEmpty {
+                    if device.uid == input.deviceID {
+                        do {
+                            try AVAudioSession.sharedInstance().setPreferredInput(device)
+                        } catch {
+                            AKLog("Could not set the preferred input to \(input)")
+                        }
                     }
-                #else
-                    // tvOS
-
-                    try AKSettings.setSession(category: .playAndRecord)
-
-                #endif
-
-                } else if AKSettings.playbackWhileMuted {
-
-                try AKSettings.setSession(category: .playback)
-
                 } else {
-                    try AKSettings.setSession(category: .ambient)
-
+                    for dataSource in device.dataSources! {
+                        if input.deviceID == "\(device.uid) \(dataSource.dataSourceName)" {
+                            do {
+                                try AVAudioSession.sharedInstance().setInputDataSource(dataSource)
+                            } catch {
+                                AKLog("Could not set the preferred input to \(input)")
+                            }
+                        }
+                    }
                 }
-            #if os(iOS)
-                try AVAudioSession.sharedInstance().setActive(true)
-            #endif
-
-            #endif
-
-            try self.engine.start()
-
-            shouldBeRunning = true
-        } catch {
-            fatalError("AudioKit: Could not start engine. error: \(error).")
+            }
         }
 
-    }
-
-    /// Stop the audio engine
-    open static func stop() {
-        // Stop the engine.
-        self.engine.stop()
-        shouldBeRunning = false
-        #if os(iOS)
-        do {
-            try AVAudioSession.sharedInstance().setActive(false)
-        } catch {
-            print("couldn't stop session \(error)")
-        }
-        #endif
-    }
-
-    // MARK: - Testing
-
-    /// Testing AKNode
-//    open static var tester: AKTester?
-
-    /// Test the output of a given node
-    ///
-    /// - Parameters:
-    ///   - node: AKNode to test
-    ///   - duration: Number of seconds to test (accurate to the sample)
-    ///
-//    open static func test(node: AKNode, duration: Double) {
-//        let samples = Int(duration * AKSettings.sampleRate)
-//        
-//        tester = AKTester(node, samples: samples)
-//        output = tester
-//        start()
-//        self.engine.pause()
-//        tester?.play()
-//        let renderer = AKOfflineRenderer(engine: self.engine)
-//        renderer?.render(Int32(samples))
-//    }
-    
-    /// Audition the test to hear what it sounds like
-    ///
-    /// - Parameters:
-    ///   - node: AKNode to test
-    ///   - duration: Number of seconds to test (accurate to the sample)
-    ///
-    open static func auditionTest(node: AKNode, duration: Double) {
-        output = node
-        start()
-        if let playableNode = node as? AKToggleable {
-            playableNode.play()
-        }
-        usleep(UInt32(duration * 1000000))
-        stop()
-        start()
-    }
-    
-    // MARK: - Configuration Change Response
-
-    // Listen to changes in audio configuration
-    // and restart the audio engine if it stops and should be playing
-    @objc fileprivate static func audioEngineConfigurationChange(_ notification: Notification) -> Void {
-
-        if (shouldBeRunning == true && self.engine.isRunning == false) {
-            do {
-                try self.engine.start()
-            } catch {
-                print("couldn't start engine after configuration change \(error)")
+        if let devices = AVAudioSession.sharedInstance().availableInputs {
+            for dev in devices {
+                if dev.uid == input.deviceID {
+                    do {
+                        try AVAudioSession.sharedInstance().setPreferredInput(dev)
+                    } catch {
+                        AKLog("Could not set the preferred input to \(input)")
+                    }
+                }
             }
         }
 
     }
 
-    // Restarts the engine after audio output has been changed, like headphones plugged in.
-    @objc fileprivate static func restartEngineAfterRouteChange(_ notification: Notification) {
-        if shouldBeRunning {
-            do {
-                try self.engine.start()
-                // Sends notification after restarting the engine, so it is safe to resume AudioKit functions.
-                if AKSettings.notificationsEnabled {
-                    NotificationCenter.default.post(
-                        name: Notification.Name(rawValue: AKNotifications.engineRestartedAfterRouteChange),
-                        object: nil,
-                        userInfo: (notification as NSNotification).userInfo)
+    // MARK: - Disconnect node inputs
 
-                }
-            } catch {
-                print("error restarting engine after route change")
-            }
-        }
-    }
-    
-    // MARK: - Deinitialization
-
-    deinit {
-        #if os(iOS)
-            NotificationCenter.default.removeObserver(
-                self,
-                name: NSNotification.Name(rawValue: AKNotifications.engineRestartedAfterRouteChange),
-                object: nil)
-        #endif
+    /// Disconnect all inputs
+    @objc open static func disconnectAllInputs() {
+        engine.disconnectNodeInput(finalMixer.avAudioNode)
     }
 }
