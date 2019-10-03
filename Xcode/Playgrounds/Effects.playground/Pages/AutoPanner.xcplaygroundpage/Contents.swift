24acd64e9de6603f09cb5ec0004aab027c67cdcd
Aurelius Prochazka <aurephd@gmail.com>
2019-10-03 14:23:20 -0700

    Using AutoPanner instead of using an operation

---
+++ Xcode/Playgrounds/Effects.playground/Pages/AutoPanner.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: ## AutoPanner
+//:
+import AudioKitPlaygrounds
+import AudioKit
+
+let file = try AKAudioFile(readFileName: playgroundAudioFiles[0])
+let player = try AKAudioPlayer(file: file)
+player.looping = true
+
+let osc = AKOscillator()
+let effect = AKAutoPanner(osc)
+
+effect.depth = 1
+effect.frequency = 1
+AudioKit.output = effect
+try AudioKit.start()
+//player.play()
+osc.start()
+
+import AudioKitUI
+
+class LiveView: AKLiveViewController {
+
+    override func viewDidLoad() {
+        addTitle("AutoPanner")
+
+        addView(AKResourcesAudioFileLoaderView(player: player, filenames: playgroundAudioFiles))
+
+        addView(AKSlider(property: "Frequency", value: effect.frequency, range: 0.1 ... 25) { sliderValue in
+            effect.frequency = sliderValue
+        })
+
+        addView(AKSlider(property: "Depth", value: effect.depth) { sliderValue in
+            effect.depth = sliderValue
+        })
+    }
+}
+
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = LiveView()
